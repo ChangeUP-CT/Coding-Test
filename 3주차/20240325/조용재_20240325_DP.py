fd57f4d3c56367e8decac0ad0f8d99385013ecbf
yyongjae <dydwo706@naver.com>
2024-03-25 23:14:59 +0900

    조용재솔

---
+++ 3주차/20240325/조용재_20240325_DP.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/2565
+
+n = int(input())
+lists = []
+dp = [1]*n
+
+for i in range(n):
+    a, b = map(int, input().split())
+    lists.append([a, b])
+
+lists.sort()
+
+for i in range(1, n):
+    for j in range(0, i):
+        if lists[j][1] < lists[i][1]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n-max(dp))
