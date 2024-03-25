4be2d506c2f79bd5f311633ddb5fa59d70e78cbb
yyongjae <dydwo706@naver.com>
2024-03-25 13:20:05 +0900

    주석

--- 2주차/20240320/조용재_20240320_구현.py
+++ 2주차/20240320/조용재_20240320_구현.py
@@ -1,7 +1,7 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/92335
-#
 import math
 
+#소수 판별 함수
 def is_prime(n):
     if n == 1:
         return False
@@ -15,10 +15,12 @@ def solution(n, k):
     rev_base = ''
 
     while n > 0:
+        # 몫과 나머지
         n, mod = divmod(n, k)
         rev_base += str(mod)
 
-    for i in list(map(int, ' '.join(rev_base[::-1].split('0')).split())):
+    # 나머지를 반대 순서로 이어붙였으니 슬라이싱으로 순서 반대로 다시 수정
+    for i in list(map(int, rev_base[::-1].split('0'))):
         if is_prime(i):
             answer += 1
 
