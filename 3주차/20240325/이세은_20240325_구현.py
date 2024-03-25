685333e2525dab9f5812cce577ea3b3de963529d
leeseeun <lse1002@hanyang.ac.kr>
2024-03-25 23:42:48 +0900

    20240325 이세은 1일1솔

--- 3주차/20240325/이세은_20240325_구현.py
+++ 3주차/20240325/이세은_20240325_구현.py
@@ -2,60 +2,68 @@
 5653. [모의 SW 역량테스트] 줄기세포배양
 출처: https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXRJ8EKe48DFAUo
 """
+import sys
+sys.stdin = open('input.txt', 'r')
+class cell:
+    def __init__(self, x, y, time):
+        self.hp=x # 비활성화에서 활성화까지 남은시간 혹은 활성화에서 죽을 때까지 시간
+        self.ahp=x # 활성화하는데 걸리는 시간
+        self.status=y # 1 비활성, 2 활성, 3 죽음, 0 없음
+        self.time=time # 생성된 시간
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+def spread(x, y, w, t):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if map1[nx][ny].status == 0:
+            map1[nx][ny].status = 1
+            map1[nx][ny].hp = w
+            map1[nx][ny].ahp = w
+            map1[nx][ny].time = t
+        elif map1[nx][ny].status == 1 and map1[nx][ny].time == t:
+            if w > map1[nx][ny].hp:
+                map1[nx][ny].hp = w
+                map1[nx][ny].ahp = w
+
 
 T = int(input())
-for test_case in range(1, 1+T):
+for tc in range(1, T+1):
     n, m, k = map(int, input().split())
-    live = [[0]*(m+2*k+2) for _ in range(n+2*k+2)]
-    check = [[0]*(m+2*k+2) for _ in range(n+2*k+2)]
+    current_time = 0
+    map1 = [[cell(0, 0, 0) for _ in range(m+k)] for _ in range(n+k)]
+    map2 = [list(map(int, input().split())) for _ in range(n)]
     for i in range(n):
-        data = list(map(int, input().split()))
         for j in range(m):
-            live[i][j] = data[j]
-            if data[j] > 0:
-                check[i][j] = -(data[j])
+            if map2[i][j] != 0:
+                map1[i + k // 2][j + k // 2].status = 1
+                map1[i + k // 2][j + k // 2].hp = map2[i][j]
+                map1[i + k // 2][j + k // 2].ahp = map2[i][j]
 
-    for _ in range(k):
-        l = [[0]*(m+2*k+2) for _ in range(n+2*k+2)]
-        for i in range(n+2*k+2):
-            for j in range(m+2*k+2):
-                if live[i][j] == 0 or live[i][j] == -1:
-                    continue
-                if check[i][j] < 0:
-                    if check[i][j] == -1:
-                        check[i][j] = live[i][j]
-                    else:
-                        check[i][j] += 1
-                elif check[i][j] > 0:
-                    x, y = i, j
-                    for p in range(4):
-                        nx = (x+dx[p]) % (n+k*2+2)
-                        ny = (y+dy[p]) % (m+k*2+2)
-                        if live[nx][ny] == -1 or live[nx][ny] > 0:
-                            continue
-                        if l[nx][ny] == 0:
-                            l[nx][ny] = live[x][y]
-                            check[nx][ny] = -live[x][y]
-                        elif l[nx][ny] < live[x][y]:
-                            l[nx][ny] = live[x][y]
-                            check[nx][ny] = -live[x][y]
-                    if check[i][j] == 1:
-                        live[i][j] = -1
-                        check[i][j] = 0
-                    else:
-                        check[i][j] -= 1
+    sero = len(map1)
+    garo = len(map1[0])
+    while current_time < k:
+        current_time += 1
+        for x in range(sero):
+            for y in range(garo):
+                if map1[x][y].status == 1 and map1[x][y].time < current_time:
+                    map1[x][y].hp -= 1
+                    if map1[x][y].hp == 0:
+                        map1[x][y].status = 2
+                        map1[x][y].hp = map1[x][y].ahp
+                        continue
+                elif map1[x][y].status == 2:
+                    map1[x][y].hp -= 1
+                    if map1[x][y].hp == map1[x][y].ahp-1:
+                        spread(x, y, map1[x][y].ahp, current_time)
+                    if map1[x][y].hp == 0:
+                        map1[x][y].status = 3
+    answer = 0
+    for i in range(sero):
+        for j in range(garo):
+            if map1[i][j].status == 1 or map1[i][j].status == 2:
+                answer += 1
 
-        for i in range(n+2*k+2):
-            for j in range(m+2*k+2):
-                if l[i][j] > 0 and live[i][j] == 0:
-                    live[i][j] = l[i][j]
-
-    cnt = 0
-    for i in range(n+2*k+2):
-        for j in range(m+2*k+2):
-            if check[i][j] != 0:
-                cnt += 1
-    print(f'#{test_case} {cnt}')
+    print(f"#{tc} {answer}")
